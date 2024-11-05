87f5317308139221631e9e4e31cb0daa166d229b
hutsulruslan <146101769+ruslanhutsul-lpnu@users.noreply.github.com>
2024-11-05 21:07:19 +0200

    Added setSimTimeViewModel method and binding of clockLabel to simDateTimeProperty in NavSceneController

--- src/main/java/pizzatimepack/NavSceneController.java
+++ src/main/java/pizzatimepack/NavSceneController.java
@@ -1,8 +1,10 @@
-package com.example.javafxsample;
+package pizzatimepack;
 
+import ViewModels.SimTimeViewModel;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
@@ -20,35 +22,65 @@ public class NavSceneController {
     private BorderPane viewBorderPane;
 
     @FXML
+    private Button cashButton;
+
+    @FXML
+    private Button kitchenButton;
+
+    @FXML
+    private Button staffButton;
+
+    @FXML
+    private Label clockLabel;
+
+    private SimTimeViewModel simTimeViewModel;
+
+    public void setSimTimeViewModel(SimTimeViewModel simTimeViewModel) {
+        this.simTimeViewModel = simTimeViewModel;
+        bindClockLabel();
+    }
+
+    private void bindClockLabel() {
+        clockLabel.textProperty().bind(simTimeViewModel.simDateTimeProperty());
+    }
+
+    @FXML
     private void toggleMenu() {
         boolean isVisible = menuItemsBox.isVisible();
 
         menuItemsBox.setVisible(!isVisible);
 
-        if (isVisible) {
-            dropDownButton.getStyleClass().remove("active-button");
-        } else {
+        double shift = menuItemsBox.getPrefHeight();
+        if (!isVisible) {
+            cashButton.setLayoutY(cashButton.getLayoutY() + shift);
+            kitchenButton.setLayoutY(kitchenButton.getLayoutY() + shift);
+            staffButton.setLayoutY(staffButton.getLayoutY() + shift);
             dropDownButton.getStyleClass().add("active-button");
+        } else {
+            cashButton.setLayoutY(cashButton.getLayoutY() - shift);
+            kitchenButton.setLayoutY(kitchenButton.getLayoutY() - shift);
+            staffButton.setLayoutY(staffButton.getLayoutY() - shift);
+            dropDownButton.getStyleClass().remove("active-button");
         }
     }
 
     @FXML
     private void btnPizza() throws IOException {
-        AnchorPane view = FXMLLoader.load(getClass().getResource("pizza-view.fxml"));
+        AnchorPane view = FXMLLoader.load(getClass().getResource("/Views/pizza-view.fxml"));
         viewBorderPane.setVisible(true);
         viewBorderPane.setCenter(view);
     }
 
     @FXML
     private void btnDrinks() throws IOException {
-        AnchorPane view = FXMLLoader.load(getClass().getResource("drinks-view.fxml"));
+        AnchorPane view = FXMLLoader.load(getClass().getResource("/Views/drinks-view.fxml"));
         viewBorderPane.setVisible(true);
         viewBorderPane.setCenter(view);
     }
 
     @FXML
     private void btnDesserts() throws IOException {
-        AnchorPane view = FXMLLoader.load(getClass().getResource("desserts-view.fxml"));
+        AnchorPane view = FXMLLoader.load(getClass().getResource("/Views/desserts-view.fxml"));
         viewBorderPane.setVisible(true);
         viewBorderPane.setCenter(view);
     }
