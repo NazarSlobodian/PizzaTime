ce231118603b46ce06df12330b6a69a7f61c4b7d
VladyslavStashenko <vladyslav.stashenko.pz.2022@lpnu.ua>
2024-11-05 22:11:58 +0200

    Added FlowGenerator class to manage order generation timing

--- src/main/java/Model/Generators/FlowGenerator.java
+++ src/main/java/Model/Generators/FlowGenerator.java
@@ -1,9 +1,42 @@
 package Model.Generators;
 
-public interface FlowGenerator {
+import java.util.Random;
+
+public class FlowGenerator {
+    private final int intervalMillis;
+    private long lastOrderTime;
+    private final Random random;
+
+    public FlowGenerator(int intervalMillis) {
+        this.intervalMillis = intervalMillis;
+        this.lastOrderTime = System.currentTimeMillis();
+        this.random = new Random();
+    }
+
     /**
-     * Decides if something should be generated based on some criteria (i.e. time passed since last generation, random number value, ...)
-     * @return true if something should be generated now, false if not
+     * Checks if a new order needs to be generated.
+     * 
+     * @return true, if the specified interval has passed, false otherwise
      */
-    boolean shouldGenerate();
+    public boolean shouldGenerateOrder() {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastOrderTime >= intervalMillis) {
+            lastOrderTime = currentTime;
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Additional opportunity with probability for order generation.
+     * 
+     * @param probability probability (from 0.0 to 1.0)
+     * @return true if an order is to be generated, false otherwise
+     */
+    public boolean shouldGenerateOrderWithProbability(double probability) {
+        if (shouldGenerateOrder()) {
+            return random.nextDouble() < probability;
+        }
+        return false;
+    }
 }
