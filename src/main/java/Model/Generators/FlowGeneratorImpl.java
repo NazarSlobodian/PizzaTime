c50a8403962dd7fdf19b0d10fbeac3174867fb0a
VladyslavStashenko <vladyslav.stashenko.pz.2022@lpnu.ua>
2024-11-07 21:10:34 +0200

    Small fix

---
+++ src/main/java/Model/Generators/FlowGeneratorImpl.java
@@ -0,0 +1,48 @@
+package Model.Generators;
+
+import java.util.Random;
+
+/**
+ * An implementation of FlowGeneratorImpl that determines when to generate a new order
+ */
+class FlowGeneratorImpl implements FlowGenerator {
+    private final int intervalMillis;
+    private long lastOrderTime;
+    private final Random random;
+
+    public FlowGeneratorImpl(int intervalMillis) {
+        this.intervalMillis = intervalMillis;
+        this.lastOrderTime = System.currentTimeMillis();
+        this.random = new Random();
+    }
+
+    /**
+     * Checks if a new order needs to be generated.
+     * 
+     * @return true, if the specified interval has passed, false otherwise
+     */
+    @Override
+    public boolean shouldGenerateOrder() {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastOrderTime >= intervalMillis) {
+            lastOrderTime = currentTime;
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * Additional opportunity with probability for order generation.
+     * 
+     * @param probability probability (from 0.0 to 1.0)
+     * @return true if an order is to be generated, false otherwise
+     */
+    @Override
+    public boolean shouldGenerateOrderWithProbability(double probability) {
+        if (shouldGenerateOrder()) {
+            return random.nextDouble() < probability;
+        }
+        return false;
+    }
+}
