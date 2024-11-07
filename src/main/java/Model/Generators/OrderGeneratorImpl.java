a2b8f943a74ce548cb57f7cd54032d80987d4f7a
NazarSlobodian <nazar.slobodian.pz.2022@lpnu.ua>
2024-11-07 16:44:51 +0200

    Split interfaces and classes

---
+++ src/main/java/Model/Generators/OrderGeneratorImpl.java
@@ -0,0 +1,54 @@
+package Model.Generators;
+
+import Model.FoodAndStuff.Cookable;
+import Model.FoodAndStuff.Menu;
+import Model.KitchenStuff.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Generates orders based on different strategies
+ */
+class OrderGeneratorImpl implements OrderGenerator {
+
+    private Menu menu;
+    private Random random;
+
+    public OrderGeneratorImpl(Menu menu) {
+        this.menu = menu;
+        this.random = new Random();
+    }
+
+    // Generates an order with a random number of pizzas (1-5)
+    @Override
+    public Order generateRandomOrder() {
+        int itemCount = random.nextInt(5) + 1;
+        List<Cookable> items = new ArrayList<>();
+        for (int i = 0; i < itemCount; i++) {
+            items.add(menu.getRandomPizza());
+        }
+        return new Order(items, System.currentTimeMillis());
+    }
+
+    // Generates an order with a fixed number of pizzas
+    @Override
+    public Order generateFixedOrder(int itemCount) {
+        List<Cookable> items = new ArrayList<>();
+        for (int i = 0; i < itemCount; i++) {
+            items.add(menu.getRandomPizza());
+        }
+        return new Order(items, System.currentTimeMillis());
+    }
+
+    // Generates promotional order with specific types of pizzas
+    @Override
+    public Order generatePromoOrder(List<String> promoPizzaTypes) {
+        List<Cookable> items = new ArrayList<>();
+        for (String type : promoPizzaTypes) {
+            items.add(menu.getPizzaByType(type));
+        }
+        return new Order(items, System.currentTimeMillis());
+    }
+}
