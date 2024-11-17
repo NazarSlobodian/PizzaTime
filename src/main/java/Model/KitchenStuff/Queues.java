d009e64c00ffef93dd548a27761eddb462fc1a33
VladyslavStashenko <vladyslav.stashenko.pz.2022@lpnu.ua>
2024-11-17 19:02:46 +0200

    Added small changes to the Queues and KitchenManager

--- src/main/java/Model/KitchenStuff/Queues.java
+++ src/main/java/Model/KitchenStuff/Queues.java
@@ -1,79 +1,73 @@
 package Model.KitchenStuff;
 
-
 import java.util.LinkedList;
 import java.util.Queue;
 
 import Model.Generators.FlowGenerator;
-import Model.Generators.OrderGenerator;
-
+import Model.Generators.OrderStrategyManager;
 
 /**
- * Manages the queues for order processing and integrates with FlowGenerator and OrderGenerator.
+ * Manages the queues for order processing and integrates with OrderStrategyManager, FlowGenerator, and KitchenManager.
  */
 public class Queues {
-    private final Queue<Order> cashierQueue;
-    private final Queue<Order> kitchenQueue;
+    private final Queue<Order> generalQueue;
     private final Queue<Order> rejectedQueue;
+    private final KitchenManager kitchenManager;
     private final long endOfDayTime;
 
     private final FlowGenerator flowGenerator;
-    private final OrderGenerator orderGenerator;
+    private final OrderStrategyManager orderStrategyManager;
 
-    public Queues(long endOfDayTime, FlowGenerator flowGenerator, OrderGenerator orderGenerator) {
-        this.cashierQueue = new LinkedList<>();
-        this.kitchenQueue = new LinkedList<>();
+    public Queues(long endOfDayTime, FlowGenerator flowGenerator, OrderStrategyManager orderStrategyManager, KitchenManager kitchenManager) {
+        this.generalQueue = new LinkedList<>();
         this.rejectedQueue = new LinkedList<>();
         this.endOfDayTime = endOfDayTime;
         this.flowGenerator = flowGenerator;
-        this.orderGenerator = orderGenerator;
+        this.orderStrategyManager = orderStrategyManager;
+        this.kitchenManager = kitchenManager;
     }
 
     /**
-     * Main logic to check and generate orders.
+     * Main logic to generate orders and manage the flow between queues and the kitchen.
      */
     public void manageOrderFlow() {
         if (flowGenerator.shouldGenerateOrder()) {
-            // Generate a new order and add it to the cashier queue
-            Order newOrder = orderGenerator.generateRandomOrder();
-            addOrderToCashierQueue(newOrder);
+            // Generate a new order using the active strategy
+            Order newOrder = orderStrategyManager.generateOrder();
+            addOrderToGeneralQueue(newOrder);
         }
+
+        // Try to process orders from the general queue into the kitchen
+        processOrdersToKitchen();
     }
 
     /**
-     * Adds an order to the cashier queue for processing.
+     * Adds an order to the general queue for processing.
      * @param order The order to be added.
      */
-    private void addOrderToCashierQueue(Order order) {
+    private void addOrderToGeneralQueue(Order order) {
         if (canOrderBeCompleted(order)) {
-            cashierQueue.add(order);
-            System.out.println("Order added to cashier queue: " + order);
+            generalQueue.add(order);
+            System.out.println("Order added to general queue: " + order);
         } else {
             rejectOrder(order);
         }
     }
 
     /**
-     * Moves an order from the cashier queue to the kitchen queue.
+     * Processes orders from the general queue and sends them to the kitchen if possible.
      */
-    public void processOrderAtCashier() {
-        if (!cashierQueue.isEmpty()) {
-            Order order = cashierQueue.poll();
-            if (order != null) {
-                kitchenQueue.add(order);
-                System.out.println("Order moved to kitchen queue: " + order);
+    private void processOrdersToKitchen() {
+        while (!generalQueue.isEmpty()) {
+            Order order = generalQueue.peek();
+            if (kitchenManager.canAcceptOrder(order)) {
+                generalQueue.poll(); // Remove the order from the general queue
+                kitchenManager.acceptOrder(order); // Pass the order to the kitchen
+                System.out.println("Order sent to kitchen: " + order);
+            } else {
+                System.out.println("Kitchen cannot accept order yet: " + order);
+                break; // Stop processing if the kitchen can't accept the current order
             }
-        }
-    }
-
-    /**
-     * Processes orders in the kitchen queue.
-     */
-    public void processOrderInKitchen() {
-        if (!kitchenQueue.isEmpty()) {
-            Order order = kitchenQueue.poll();
-            // Logic to handle order preparation (can be added later)
-            System.out.println("Order prepared in kitchen: " + order);
         }
     }
 
@@ -83,7 +77,6 @@ public class Queues {
      * @return True if the order can be completed, false otherwise.
      */
     private boolean canOrderBeCompleted(Order order) {
-        // Assume each order has an average preparation time (can be adjusted)
         long estimatedCompletionTime = order.getOrderTime() + 20 * 60 * 1000; // 20 minutes
         return estimatedCompletionTime <= endOfDayTime;
     }
