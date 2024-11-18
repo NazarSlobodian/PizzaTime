2e5aa31dabe75a2eadc2a6e969e9d6ad80914ec8
VladyslavStashenko <vladyslav.stashenko.pz.2022@lpnu.ua>
2024-11-18 13:20:39 +0200

    Added changes to Queues and Order
    1.Replacing a single queue with several queues (queue based on order size).
    2.List of all orders (allOrders).
    3.Copying orders before sending them to the kitchen (processOrdersToKitchen).
    4.Distribution of orders by queues (addOrderToAppropriateQueue).

--- src/main/java/Model/KitchenStuff/Queues.java
+++ src/main/java/Model/KitchenStuff/Queues.java
@@ -1,6 +1,10 @@
 package Model.KitchenStuff;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 
 import Model.Generators.FlowGenerator;
@@ -12,8 +16,9 @@ import Model.Utils.Schedule;
  * Manages the queues for order processing and integrates with OrderStrategyManager, FlowGenerator, and KitchenManager.
  */
 public class Queues {
-    private final Queue<Order> generalQueue;
+    private final Map<String, Queue<Order>> orderQueues;
     private final Queue<Order> rejectedQueue;
+    private final List<Order> allOrders;
     private final KitchenManager kitchenManager;
     Schedule  schedule;
 
@@ -22,13 +27,23 @@ public class Queues {
     Clock clock;
 
     public Queues(Schedule schedule, FlowGenerator flowGenerator, OrderStrategyManager orderStrategyManager, KitchenManager kitchenManager, Clock clock) {
-        this.generalQueue = new LinkedList<>();
+        this.orderQueues = new HashMap<>();
         this.rejectedQueue = new LinkedList<>();
+        this.allOrders = new ArrayList<>();
         this.schedule = schedule;
         this.flowGenerator = flowGenerator;
         this.orderStrategyManager = orderStrategyManager;
         this.kitchenManager = kitchenManager;
         this.clock = clock;
+    }
+
+    /**
+     * Adds a new queue for a specific type of order.
+     *
+     * @param queueName The name of the queue.
+     */
+    public void addQueue(String queueName) {
+        orderQueues.put(queueName, new LinkedList<>());
     }
 
     /**
@@ -38,45 +53,57 @@ public class Queues {
         if (flowGenerator.shouldGenerateOrder()) {
             // Generate a new order using the active strategy
             Order newOrder = orderStrategyManager.generateOrder();
-            addOrderToGeneralQueue(newOrder);
+            addOrderToAppropriateQueue(newOrder);
         }
 
-        // Try to process orders from the general queue into the kitchen
+        // Try to process orders from all queues into the kitchen
         processOrdersToKitchen();
     }
 
     /**
-     * Adds an order to the general queue for processing.
+     * Adds an order to the appropriate queue based on custom criteria.
+     *
      * @param order The order to be added.
      */
-    private void addOrderToGeneralQueue(Order order) {
-        if (canOrderBeCompleted(order)) {
-            generalQueue.add(order);
-            System.out.println("Order added to general queue: " + order);
+    private void addOrderToAppropriateQueue(Order order) {
+        allOrders.add(order); // Save to the history of all orders
+
+        // Example logic: decide the queue based on order size
+        String queueName = (order.getItems().size() <= 3) ? "SmallOrders" : "LargeOrders";
+        Queue<Order> queue = orderQueues.get(queueName);
+
+        if (queue != null && canOrderBeCompleted(order)) {
+            queue.add(order);
+            System.out.println("Order added to queue [" + queueName + "]: " + order);
         } else {
             rejectOrder(order);
         }
     }
 
     /**
-     * Processes orders from the general queue and sends them to the kitchen if possible.
+     * Processes orders from all queues and sends them to the kitchen if possible.
      */
     private void processOrdersToKitchen() {
-        while (!generalQueue.isEmpty()) {
-            Order order = generalQueue.peek();
-            if (kitchenManager.canAcceptOrder(order)) {
-                generalQueue.poll(); // Remove the order from the general queue
-                kitchenManager.acceptOrder(order); // Pass the order to the kitchen
-                System.out.println("Order sent to kitchen: " + order);
-            } else {
-                System.out.println("Kitchen cannot accept order yet: " + order);
-                break; // Stop processing if the kitchen can't accept the current order
+        for (Map.Entry<String, Queue<Order>> entry : orderQueues.entrySet()) {
+            Queue<Order> queue = entry.getValue();
+            while (!queue.isEmpty()) {
+                Order order = queue.peek();
+                if (kitchenManager.canAcceptOrder(order)) {
+                    queue.poll(); // Remove the order from the queue
+                    Order orderCopy = new Order(order); // Create a copy before sending
+                    kitchenManager.acceptOrder(orderCopy);
+                    System.out.println("Order sent to kitchen from [" + entry.getKey() + "]: " + orderCopy);
+                } else {
+                    System.out.println("Kitchen cannot accept order yet from queue [" + entry.getKey() + "]: " + order);
+                    break; // Stop processing this queue if the kitchen can't accept the current order
+                }
             }
         }
     }
 
     /**
      * Checks if the order can be completed before the end of the day.
+     *
      * @param order The order to check.
      * @return True if the order can be completed, false otherwise.
      */
@@ -87,6 +114,7 @@ public class Queues {
 
     /**
      * Rejects the order if it can't be completed on time.
+     *
      * @param order The order to be rejected.
      */
     private void rejectOrder(Order order) {
@@ -96,9 +124,19 @@ public class Queues {
 
     /**
      * Retrieves the queue of rejected orders.
+     *
      * @return The rejected orders queue.
      */
     public Queue<Order> getRejectedOrders() {
         return rejectedQueue;
+    }
+
+    /**
+     * Retrieves the list of all orders processed throughout the day.
+     *
+     * @return The list of all orders.
+     */
+    public List<Order> getAllOrders() {
+        return allOrders;
     }
 }
