81207a1007b53f05c635e0bafd10619461502577
hutsulruslan <146101769+ruslanhutsul-lpnu@users.noreply.github.com>
2024-11-20 22:09:48 +0200

    The KitchenSceneController file has been created. Added a binding to display the process of cooking pizzas to order.

---
+++ src/main/java/pizzatimepack/KitchenSceneController.java
@@ -0,0 +1,37 @@
+package pizzatimepack;
+
+import ViewModels.KitchenPizzaViewModel;
+import ViewModels.KitchenViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class KitchenSceneController {
+
+    @FXML
+    private TableView<KitchenPizzaViewModel> kitchenTable;
+
+    @FXML
+    private TableColumn<KitchenPizzaViewModel, String> orderNumberColumn;
+
+    @FXML
+    private TableColumn<KitchenPizzaViewModel, String> statusColumn;
+
+    @FXML
+    private TableColumn<KitchenPizzaViewModel, String> readinessColumn;
+
+    private KitchenViewModel kitchenViewModel;
+
+    public void setKitchenViewModel(KitchenViewModel kitchenViewModel) {
+        this.kitchenViewModel = kitchenViewModel;
+        kitchenTable.setItems(kitchenViewModel.getPizzasInKitchen());
+    }
+
+    @FXML
+    public void initialize() {
+        orderNumberColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+        statusColumn.setCellValueFactory(cellData -> cellData.getValue().stateProperty());
+        readinessColumn.setCellValueFactory(cellData -> cellData.getValue().readinessProperty());
+    }
+}
